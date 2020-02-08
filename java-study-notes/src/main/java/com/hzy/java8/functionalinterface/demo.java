1faab526d4e756a13fc98b3fa1a7663aa3a03715
hzy <welcome_hzy@163.com>
2020-02-08 16:52:03 +0800

    java8

---
+++ java-study-notes/src/main/java/com/hzy/java8/functionalinterface/demo.java
@@ -0,0 +1,37 @@
+package com.hzy.java8.functionalinterface;
+
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * java8 提供了 4大核心函数是接口
+ * <p>
+ * Consumer<T>  消费型接口
+ * void accept(T t);
+ * <p>
+ * Supplier<T>  供给型接口
+ * T get();
+ * <p>
+ * Function<T, R>  函数型接口
+ * R apply(T t);
+ * <p>
+ * Predicate<T>  断言型接口
+ * boolean test(T t);
+ */
+public class demo {
+
+    //消费型接口
+    @Test
+    public void test01() {
+        method1(1000, (m) -> System.out.println(m));
+    }
+
+    public void method1(Integer arg, Consumer<Integer> consumer) {
+        consumer.accept(arg);
+    }
+
+
+    //供给型接口
+
+}
