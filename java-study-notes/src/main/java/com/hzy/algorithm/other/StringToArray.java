c82ae9bba9501d5450ffe2e4efee6a93af07d6c1
hzy <welcome_hzy@163.com>
2020-02-28 21:50:59 +0800

    小练习

---
+++ java-study-notes/src/main/java/com/hzy/algorithm/other/StringToArray.java
@@ -0,0 +1,13 @@
+package com.hzy.algorithm.other;
+
+public class StringToArray {
+
+    public static void main(String[] args) {
+        String[] splitArray = ",qwer ,qwerq, qwerq re ，qweqeqwr,e r".replace(" ","").split(",|，");
+        System.out.println(splitArray.length);
+        for (String s : splitArray) {
+            System.out.println("====="+s+"=====");
+        }
+    }
+
+}
