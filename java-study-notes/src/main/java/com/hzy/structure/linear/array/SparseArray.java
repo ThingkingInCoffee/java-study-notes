7ccf2ea59989e6639361d5c3eda80aabe6b41458
hzy <huangzhongyao@opechk.com>
2020-12-14 19:24:58 +0800

    稀疏数组

--- java-study-notes/src/main/java/com/hzy/structure/linear/array/SparseArray.java
+++ java-study-notes/src/main/java/com/hzy/structure/linear/array/SparseArray.java
@@ -1,17 +1,19 @@
 package com.hzy.structure.linear.array;
 
+import java.io.*;
+
 /**
  * 稀疏数组
  * 稀疏数组把原数组中具有的不同值的元素的行列及值记录在一个小规模的数组中，从而缩小程序的规模
  * 稀疏数组也是二维数组，行数由原数组的数据决定，列数一般为 3 列
  * 稀疏数组的第一行记录原数组一共有几行几列，有多少个不为零的值
- *      第一列：原数组的行数
- *      第二列：原数组的列数
- *      第三列：原数组有多少个不为零的值
+ * 第一列：原数组的行数
+ * 第二列：原数组的列数
+ * 第三列：原数组有多少个不为零的值
  * 之后的行记录原数组中不为 0 (或其他默认值)的值(x)所在的行数、列数以及 x 的值
- *      第一列：x 在原数组中的行数
- *      第二列：x 在原数组中的列数
- *      第三列：x 的值
+ * 第一列：x 在原数组中的行数
+ * 第二列：x 在原数组中的列数
+ * 第三列：x 的值
  */
 public class SparseArray {
 
@@ -25,6 +27,7 @@ public class SparseArray {
 
     public static void main(String[] args) {
         int[][] array = getArray();
+        createFile(array);
         System.out.println("获取的原始数组为");
         printArray(array);
         // 生成稀疏数组
@@ -35,10 +38,14 @@ public class SparseArray {
         int[][] originArray = parseSparseArray(sparseArray);
         System.out.println("获取的原始数组为");
         printArray(originArray);
+        // 解析文件
+        parseFile();
+
     }
 
     /**
-     *  生成稀疏数组
+     * 生成稀疏数组
+     *
      * @param sourceArray
      * @return
      */
@@ -76,11 +83,12 @@ public class SparseArray {
 
 
     /**
-     *  解析稀疏数组
+     * 解析稀疏数组
+     *
      * @param sparseArray
      * @return
      */
-    public static int[][] parseSparseArray(int[][] sparseArray){
+    public static int[][] parseSparseArray(int[][] sparseArray) {
         int[][] originArray = null;
         for (int i = 0; i < sparseArray.length; i++) {
             int row = sparseArray[i][0];
@@ -99,7 +107,8 @@ public class SparseArray {
 
 
     /**
-     *  打印数组
+     * 打印数组
+     *
      * @param chessArray
      */
     public static void printArray(int[][] chessArray) {
@@ -108,6 +117,41 @@ public class SparseArray {
                 System.out.printf("%d\t", data);
             }
             System.out.println();
+        }
+    }
+
+    public static void createFile(int[][] arrayData) {
+        File file = new File("F:\\map.data");
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file);
+            for (int[] row : arrayData) {
+                for (int data : row) {
+                    outputStream.write(Integer.toString(data).getBytes());
+                }
+                outputStream.write("\r\n".getBytes());
+            }
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void parseFile() {
+        File file = new File("F:\\map.data");
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));//构造一个BufferedReader类来读取文件
+            String s = null;
+            while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+                System.out.println(s);
+                stringBuilder.append(s);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
