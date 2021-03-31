cc148a66249e63702b585cff123f175b73b9efed
hzy <huangzhongyao@opechk.com>
2021-03-31 15:01:42 +0800

    单链表题目

--- java-study-notes/src/main/java/com/hzy/structure/linear/linkedList/SingleLinkListInterviewQuestion.java
+++ java-study-notes/src/main/java/com/hzy/structure/linear/linkedList/SingleLinkListInterviewQuestion.java
@@ -1,9 +1,11 @@
 package com.hzy.structure.linear.linkedList;
 
+import java.util.Stack;
+
 /**
  * 面试题目
  */
-public class LinkListInterviewQuestion {
+public class SingleLinkListInterviewQuestion {
 
     public static void main(String[] args) {
         LinkedNode node01 = new LinkedNode(1, "20001");
@@ -23,6 +25,15 @@ public class LinkListInterviewQuestion {
         System.out.println(nodeNum);
 
         getSpecialNodeDesc(linkedList.getHead(), 3);
+
+        System.out.println("开始反转");
+        reverseLinkedList(linkedList.getHead());
+        System.out.println("反转结果：");
+        linkedList.list();
+
+        System.out.println("栈结构反转结果：");
+        // 使用栈反转链表
+        reversePrint(linkedList.getHead());
     }
 
 
@@ -47,6 +58,7 @@ public class LinkListInterviewQuestion {
     }
 
     /**
+     * 题目 2
      * 获取倒数指定位置的节点内容
      * 1-2-3-4-5-6-7-8-9
      * 倒数第 index 个 如 倒数3个， 则需要移动的指针次数 为 6
@@ -71,6 +83,63 @@ public class LinkListInterviewQuestion {
         }
         System.out.println("找到的节点是" + temp);
         return temp;
+    }
+
+    /**
+     * 题目 3
+     * 单链表反转
+     *
+     * @param head
+     * @return
+     */
+    public static void reverseLinkedList(LinkedNode head) {
+        if (head.next == null) {
+            System.out.println("空的");
+            return;
+        }
+        LinkedNode reverseNodeHead = new LinkedNode(0, "");
+        LinkedNode source = head.next;
+        LinkedNode nextNode;
+        while (source != null) {
+            nextNode = source.next;  // 保存原链表节点的下一节点数据
+            source.next = reverseNodeHead.next;  // 将反转链表head后的所有节点追加到当前传入节点
+            reverseNodeHead.next = source; // 将追加了反转链表的原数据节点追加到反转链表头节点后面
+            source = nextNode; // 原始节点指向下一个节点
+        }
+        head.next = reverseNodeHead.next;
+    }
+
+    /**
+     * 题目 4
+     * 倒叙遍历单链表
+     * 使用栈的结构，先进后出
+     *
+     * @param head
+     */
+    public static void reversePrint(LinkedNode head) {
+        if (head.next == null) {
+            System.out.println("空的");
+            return;
+        }
+        LinkedNode current = head.next;
+        Stack<LinkedNode> stack = new Stack<>();
+        while (current != null) {
+            stack.add(current);
+            current = current.next;
+        }
+        for (LinkedNode node : stack) {
+            System.out.println(node);
+        }
+    }
+
+    /**
+     *  题目 5
+     *  合并两个单链表，并排序
+     * @param first
+     * @param second
+     */
+    public static void mergeAndSortTwoLinkedList(LinkedNode first, LinkedNode second) {
+
     }
 
 
