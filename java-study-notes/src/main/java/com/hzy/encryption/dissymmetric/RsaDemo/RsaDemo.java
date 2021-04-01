2e9bccc6ea59c03c4aefc7cfaa044baca9215230
hzy <welcome_hzy@163.com>
2021-04-01 22:06:01 +0800

    合并两个有序链表

--- java-study-notes/src/main/java/com/hzy/encryption/dissymmetric/RsaDemo/RsaDemo.java
+++ java-study-notes/src/main/java/com/hzy/encryption/dissymmetric/RsaDemo/RsaDemo.java
@@ -1,56 +1,56 @@
-package com.hzy.encryption.dissymmetric.RsaDemo;
-
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.*;
-
-public class RsaDemo {
-
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, Base64DecodingException {
-        // 原文
-        String sourceString = "加密前的文字123abc";
-        // 使用的算法
-        String algorithm = "RSA";
-        // 密钥对生成器
-        KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm); // 异常抛出
-        // 生成密钥对
-        KeyPair keyPair = generator.generateKeyPair();
-        // 私钥
-        PrivateKey privateKey = keyPair.getPrivate();
-        // 公钥
-        PublicKey publicKey = keyPair.getPublic();
-        // 私钥字节数组
-        byte[] privateKeyEncoded = privateKey.getEncoded();
-        // 公钥字节数组
-        byte[] publicKeyEncoded = publicKey.getEncoded();
-        // base64 转码 防止乱码
-        String privateKeyEncodedString = Base64.encode(privateKeyEncoded);
-        String publicKeyEncodedString = Base64.encode(publicKeyEncoded);
-        // 打印公钥私钥
-        System.out.println("privateKeyEncodedString = "+privateKeyEncodedString);
-        System.out.println("publicKeyEncodedString = "+publicKeyEncodedString);
-
-        // 创建加密对象
-        Cipher cipher = Cipher.getInstance(algorithm); // 异常抛出
-        // 加密初始化
-        // 参数一 处理模式 加密或解密
-        // 参数二 处理使用的密钥，公钥或私钥
-        cipher.init(Cipher.ENCRYPT_MODE, privateKey); //异常抛出
-        // 加密处理
-        byte[] bytes = cipher.doFinal(sourceString.getBytes());//异常抛出
-        // base64 转码
-        String encode = Base64.encode(bytes);
-        System.out.println("encode result = "+encode);
-
-        // 解密
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
-        byte[] decode = Base64.decode(encode); //异常抛出
-        byte[] sourceBytes = cipher.doFinal(decode);
-        System.out.println("sourceString = "+new String(sourceBytes));
-    }
-}
+//package com.hzy.encryption.dissymmetric.RsaDemo;
+//
+//import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+//import com.sun.org.apache.xml.internal.security.utils.Base64;
+//
+//import javax.crypto.BadPaddingException;
+//import javax.crypto.Cipher;
+//import javax.crypto.IllegalBlockSizeException;
+//import javax.crypto.NoSuchPaddingException;
+//import java.security.*;
+//
+//public class RsaDemo {
+//
+//    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, Base64DecodingException {
+//        // 原文
+//        String sourceString = "加密前的文字123abc";
+//        // 使用的算法
+//        String algorithm = "RSA";
+//        // 密钥对生成器
+//        KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm); // 异常抛出
+//        // 生成密钥对
+//        KeyPair keyPair = generator.generateKeyPair();
+//        // 私钥
+//        PrivateKey privateKey = keyPair.getPrivate();
+//        // 公钥
+//        PublicKey publicKey = keyPair.getPublic();
+//        // 私钥字节数组
+//        byte[] privateKeyEncoded = privateKey.getEncoded();
+//        // 公钥字节数组
+//        byte[] publicKeyEncoded = publicKey.getEncoded();
+//        // base64 转码 防止乱码
+//        String privateKeyEncodedString = Base64.encode(privateKeyEncoded);
+//        String publicKeyEncodedString = Base64.encode(publicKeyEncoded);
+//        // 打印公钥私钥
+//        System.out.println("privateKeyEncodedString = "+privateKeyEncodedString);
+//        System.out.println("publicKeyEncodedString = "+publicKeyEncodedString);
+//
+//        // 创建加密对象
+//        Cipher cipher = Cipher.getInstance(algorithm); // 异常抛出
+//        // 加密初始化
+//        // 参数一 处理模式 加密或解密
+//        // 参数二 处理使用的密钥，公钥或私钥
+//        cipher.init(Cipher.ENCRYPT_MODE, privateKey); //异常抛出
+//        // 加密处理
+//        byte[] bytes = cipher.doFinal(sourceString.getBytes());//异常抛出
+//        // base64 转码
+//        String encode = Base64.encode(bytes);
+//        System.out.println("encode result = "+encode);
+//
+//        // 解密
+//        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+//        byte[] decode = Base64.decode(encode); //异常抛出
+//        byte[] sourceBytes = cipher.doFinal(decode);
+//        System.out.println("sourceString = "+new String(sourceBytes));
+//    }
+//}
--- java-study-notes/src/main/java/com/hzy/structure/linear/linkedList/SingleLinkListInterviewQuestion.java
+++ java-study-notes/src/main/java/com/hzy/structure/linear/linkedList/SingleLinkListInterviewQuestion.java
@@ -34,6 +34,23 @@ public class SingleLinkListInterviewQuestion {
         System.out.println("栈结构反转结果：");
         // 使用栈反转链表
         reversePrint(linkedList.getHead());
+        System.out.println("测试合并==========");
+        LinkedNode node011 = new LinkedNode(1, "20001");
+        LinkedNode node033 = new LinkedNode(3, "20003");
+        LinkedNode node044 = new LinkedNode(4, "20004");
+        LinkedNode node055 = new LinkedNode(5, "20005");
+        LinkedNode node066 = new LinkedNode(6, "20006");
+        LinkedNode node099 = new LinkedNode(9, "20009");
+
+        SingleLinkedList linkedList1 = new SingleLinkedList();
+        linkedList1.addNode(node011);
+        linkedList1.addNode(node044);
+        linkedList1.addNode(node055);
+        SingleLinkedList linkedList2 = new SingleLinkedList();
+        linkedList2.addNode(node033);
+        linkedList2.addNode(node066);
+        linkedList2.addNode(node099);
+        mergeAndSortTwoLinkedList(linkedList1.getHead(), linkedList2.getHead());
     }
 
 
@@ -133,13 +150,39 @@ public class SingleLinkListInterviewQuestion {
     }
 
     /**
-     *  题目 5
-     *  合并两个单链表，并排序
-     * @param first
-     * @param second
+     * 题目 5
+     * 合并两个单链表，并排序
+     *
+     * @param first  链表一的head节点
+     * @param second 链表二的节点
      */
     public static void mergeAndSortTwoLinkedList(LinkedNode first, LinkedNode second) {
-
+        SingleLinkedList list = new SingleLinkedList();
+        LinkedNode temp = list.getHead();
+        LinkedNode firstTemp = first.next;
+        LinkedNode secondTemp = second.next;
+        while (true) {
+            if (firstTemp == null && secondTemp == null) {
+                System.out.println("都空了");
+                break;
+            }
+            if (firstTemp == null) {
+                temp.next = secondTemp;
+                secondTemp = secondTemp.next;
+            } else if (secondTemp == null) {
+                temp.next = firstTemp;
+                firstTemp = firstTemp.next;
+            } else if (firstTemp.code < secondTemp.code) {
+                temp.next = firstTemp;
+                firstTemp = firstTemp.next;
+            } else {
+                temp.next = secondTemp;
+                secondTemp = secondTemp.next;
+            }
+            temp = temp.next;
+        }
+        System.out.println("结束---------------");
+        list.list();
     }
 
 
